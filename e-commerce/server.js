6ca9e2d5650bbcde9c48d79f2257085fa5d038fe
tayyaba710 <tayyabakouser710@gmail.com>
2024-05-21 11:19:15 +0400

    updating with server.js

--- e-commerce/server.js
+++ e-commerce/server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const admin = require('firebase-admin');
+// const admin = require('firebase-admin');
 const path = require('path');
 const bcrypt = require('bcrypt');
 
